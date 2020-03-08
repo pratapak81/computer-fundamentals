ae40dad7f21dcb7dd5f1aecc8324ef55d9dab5f4
Pratap A K <pratap.ak@ge.com>
2020-03-08 09:52:39 +0530

    feat: implemented Queue data structure

---
+++ data-structures/src/main/java/com/nsc/datastructures/queue/BasicQueue.java
@@ -0,0 +1,98 @@
+package com.nsc.datastructures.queue;
+
+import java.util.StringJoiner;
+
+public class BasicQueue<T> {
+    private T[] data;
+    private int front;
+    private int end;
+
+    public BasicQueue() {
+        this(100);
+    }
+
+    @SuppressWarnings("unchecked")
+    public BasicQueue(int size) {
+        data = (T[]) new Object[size];
+        front = end = -1;
+    }
+
+    public int size() {
+        if (front == -1 && end == -1) {
+            return 0;
+        }
+        return end - front + 1;
+    }
+
+    // ensure capacity before adding
+    public void enQueue(T element) {
+        if ((end + 1) % data.length == front) {
+            throw new IndexOutOfBoundsException("Queue is full");
+        }
+
+        if (size() == 0) {
+            front++;
+            end++;
+            data[end] = element;
+        } else {
+            end++;
+            data[end] = element;
+        }
+    }
+
+    // set the removed element null so that it will be eligible for Garbage Collection. Same as PriorityQueue in Java
+    public T deQueue() {
+        T element = null;
+        if (size() == 0) {
+            throw new UnsupportedOperationException("Queue is empty");
+        } else if (end == front) {
+            element = data[front];
+            data[front] = null;
+            front = -1;
+            end = -1;
+        } else {
+            element = data[front];
+            data[front] = null;
+            front++;
+        }
+        return element;
+    }
+
+    public boolean contains(T element) {
+        if (size() == 0) {
+            return false;
+        }
+        for (int i = front; i <= end; i++) {
+            if (data[i].equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T access(int index) {
+        if (size() == 0 || index > size()) {
+            throw new RuntimeException("No item in the Queue or index out of bound");
+        }
+
+        int trueIndex = 0;
+        for (int i = front; i <= end; i++) {
+            if (trueIndex == index) {
+                return data[i];
+            }
+            trueIndex++;
+        }
+        throw new RuntimeException("can't get item at the given position");
+    }
+
+    // dummy method to print all the elements
+    public void printAll() {
+        System.out.print("[");
+        StringJoiner stringJoiner = new StringJoiner(", ");
+        for (int i = front; i <= end; i++) {
+            stringJoiner.add((String) data[i]);
+        }
+        System.out.print(stringJoiner.toString());
+        System.out.println("]");
+    }
+}
---
+++ data-structures/src/main/java/com/nsc/datastructures/queue/QueueExample.java
@@ -0,0 +1,46 @@
+package com.nsc.datastructures.queue;
+
+/**
+ * A Queue is a linear structure which follows a particular order in which the operations are performed. The order is First In First Out (FIFO)
+ * A Queue is an ordered line or sequence
+ * <p>
+ * Queue Characteristics:
+ * 1. enQueue (insert)
+ * 2. deQueue (delete)
+ * 3. Search
+ * 4. Access
+ * <p>
+ * Note: most of the Queue implementations doesn't allow null values.
+ * <p>
+ * Core Java Queues:
+ * 1. Queue<E>
+ * ArrayBLockingQueue, ArrayDeque, ConcurrentLinkedDeque, ConcurrentLinkedQueue, DelayQueue, LinkedBlockingDeque,
+ * LinkedBlockingQueue, LinkedList, LinkedTransferQueue, PriorityBlockingQueue, PriorityQueue, SynchronousQueue
+ * https://docs.oracle.com/javase/tutorial/collections/implementations/queue.html
+ */
+public class QueueExample {
+    public static void main(String[] args) {
+        BasicQueue<String> basicQueue = new BasicQueue<>();
+
+        basicQueue.enQueue("Pratap");
+        basicQueue.enQueue("AK");
+        basicQueue.enQueue("GE");
+
+        System.out.println("Size: " + basicQueue.size());
+        basicQueue.printAll();
+
+        System.out.println("GE exist? " + basicQueue.contains("GE"));
+        System.out.println("UNKNOWN exist? " + basicQueue.contains("UNKNOWN"));
+
+        System.out.println("element at index 2 is " + basicQueue.access(2));
+        System.out.println("element at index 0 is " + basicQueue.access(0));
+
+        System.out.println("Removed " + basicQueue.deQueue());
+        ;
+        System.out.println("Size: " + basicQueue.size());
+        basicQueue.printAll();
+
+        System.out.println("element at index 0 is now  " + basicQueue.access(0));
+
+    }
+}
